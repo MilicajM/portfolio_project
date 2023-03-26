b3c240f3429ade7249bfbedb90c321db827ade26
Martin Milicaj <Martin.milicaj@gmail.com>
2023-03-25 20:43:01 -0400

    created model for blogs and created home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+# title
+# publication date
+# body
+# image
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# add the blog app
+
+# create a migration
+
+# migrate
+
+# add to the admin
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "blog.apps.BlogConfig",
     "projects.apps.ProjectsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -76,8 +77,12 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "portfoliodb",
+        "USER": "postgres",
+        "PASSWORD": "Snowba11",
+        "HOST": "localhost",
+        'PORT': "5432",
     }
 }
 
